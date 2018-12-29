053ff5839653ac1ee1613120f163008ded8f4f12
Fabian Marin <fabian-guitar@hotmail.com>
2018-12-29 01:37:25 -0500

    todo template

--- src/App.js
+++ src/App.js
@@ -9,21 +9,50 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              
+            </span>
           </a>
-        </header>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="card mt-4">
+                    <div className="card-title text-center">
+                      <h3>Título</h3>
+                      <span className="badge badge-pill badge-danger ml-2">
+                        terminada?
+                      </span>
+                    </div>
+                    <div className="card-body">
+                      descripción
+                    </div>
+                    <div className="card-footer">
+                      <button
+                        className="btn btn-danger">
+                        Eliminar
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }

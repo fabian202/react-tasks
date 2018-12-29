fb2d1f37b869035a2915d7555613990458c17768
Fabian Marin <fabian-guitar@hotmail.com>
2018-12-29 01:26:41 -0500

    static todoes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import  { todos } from './data/todos.json';
+console.log(todos);
+
 class App extends Component {
   render() {
     return (
